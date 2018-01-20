c617ccf6efe017398c7b56163c0d9f6af51e3584
ac-2017 <exceedhero@gmail.com>
2018-01-20 13:47:11 -0500

    Added easter egg

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,8 @@ class App extends React.Component {
       startClickPos: {},
       endClickPos: {},
       showModal: false,
-      box: ''
+      box: '',
+      raining: false
     }
     this.createWorld = this.createWorld.bind(this)
     this.Engine = Matter.Engine
@@ -31,12 +32,20 @@ class App extends React.Component {
     this.bulletTime = this.bulletTime.bind(this)
     this.handleBulletTime = this.handleBulletTime.bind(this)
     this.handleModal = this.handleModal.bind(this)
+    this.addBlocks = this.addBlocks.bind(this)
+    this.handleReload = this.handleReload.bind(this)
+    this.heavyRain = this.heavyRain.bind(this)
+    this.toggleRain = this.toggleRain.bind(this)
+
+    this.renderr = null;
+    this.mouse = null;
+    this.mouseConstraint = null;
   }
 
   componentDidMount() {
     this.createWorld()
     $(window).resize(() => {
-      this.createWorld()
+      this.handleReload()
     })
   }
   handleClick(box) {
@@ -60,6 +69,19 @@ class App extends React.Component {
       this.bulletTime(this.engine)
     }
   }
+  handleReload() {
+    this.World.clear(this.engine.world, false)
+    this.addBlocks()
+  }
+  toggleRain() {
+    console.log('toggle')
+    this.setState({
+      raining: !this.state.raining
+    })
+    setTimeout(() => {
+      this.heavyRain()
+    },200)
+  }
   bulletTime(engine) {
     var bodies = this.Composite.allBodies(engine.world);
     for (var i = 0; i < bodies.length; i++) {
@@ -73,10 +95,10 @@ class App extends React.Component {
       }
     }
     setTimeout(() => {
-      this.engine.world.gravity.scale /= 10
-    },300)
+      this.engine.timing.timeScale = .3
+    },200)
     setTimeout(() => {
-      this.engine.world.gravity.scale *=10
+      this.engine.timing.timeScale = 1
     },1500)
     setTimeout(() => {
       this.setState({
@@ -84,27 +106,38 @@ class App extends React.Component {
       })
     },2500)
   }
+  heavyRain() {
+    let width = $(window).width()
+    let rainDrop = this.Bodies.circle( (Math.floor(Math.random() * Math.floor(width)) ), -1, 10, {
+      render: {
+        sprite: {
+          texture: 'https://www.shareicon.net/data/512x512/2015/10/07/113639_drop_512x512.png',
+          xScale: 10/512,
+          yScale: 10/512
+        }
+      }
+    })
+    this.World.add(this.engine.world, [rainDrop])
+    if (this.state.raining) {
+      setTimeout(() => {
+        this.heavyRain()
+      },100)
+    }
+  }
   createWorld() {
     $("canvas").remove();
-    // let Engine = Matter.Engine,
-    //     Render = Matter.Render,
-    //     World = Matter.World,
-    //     Bodies = Matter.Bodies,
-    //     Mouse = Matter.Mouse,
-    //     MouseConstraint = Matter.MouseConstraint;
     this.engine.events = {};
     this.World.clear(this.engine.world);
     this.Engine.clear(this.engine);
     this.engine = this.Engine.create()
-    this.engine.world.gravity.scale *=5
-
+    this.engine.world.gravity.scale *=7
     let width = $(window).width();
     let height = $(window).height();
     let nameWidth = $('.name').width();
     let nameHeight = $('.name').height();
     let vmin = Math.min(width, height)
     let boxWidth = vmin*.175
-    let render = this.Render.create({
+    this.renderr = this.Render.create({
         element: document.body,
         engine: this.engine,
         options: {
@@ -114,13 +147,52 @@ class App extends React.Component {
           width: width-1
         }
     });
+    this.mouse = this.Mouse.create(this.renderr.canvas)
+    this.mouseConstraint = this.MouseConstraint.create(this.engine, {
+        mouse: this.mouse,
+          constraint: {
+            stiffness: 0.2,
+            render: {
+              visible: false
+            }
+          }
+      });
+    this.Events.on(this.mouseConstraint, "startdrag", (e) => {
+      this.setState({
+        startClickPos: {x: e.mouse.absolute.x, y: e.mouse.absolute.y}
+      })
+    })
+    this.Events.on(this.mouseConstraint, "enddrag", (e) => {
+      this.setState({
+        endClickPos: {x: e.mouse.absolute.x, y: e.mouse.absolute.y}
+      })
+      this.handleClick(e.body.label)
+    })
+    this.Engine.run(this.engine);
+    this.Render.run(this.renderr);
+    this.addBlocks()
+  }
+  addBlocks() {
+
+    let width = $(window).width();
+    let height = $(window).height();
+    let nameWidth = $('.name').width();
+    let nameHeight = $('.name').height();
+    let vmin = Math.min(width, height)
+    let boxWidth = vmin*.175
+
+    this.renderr.options.width = width
+    this.renderr.options.height= height
+    this.renderr.canvas.width=width
+    this.renderr.canvas.height = height
+
     let ground = this.Bodies.rectangle(width/2, height+5, width, 5, {
       isStatic: true
     })
-    let leftWall = this.Bodies.rectangle(-2, height/2, 2, height*10, {
+    let leftWall = this.Bodies.rectangle(-25, height/2, 50, height*10, {
       isStatic: true
     })
-    let rightWall = this.Bodies.rectangle(width, height/2, 2, height*10, {
+    let rightWall = this.Bodies.rectangle(width+25, height/2, 50, height*10, {
       isStatic: true
     })
     let ceiling = this.Bodies.rectangle(width/2, -height/2, width*2, 5, {
@@ -138,9 +210,10 @@ class App extends React.Component {
         fillStyle: 'transparent'
       }
     })
-    let boxJS = this.Bodies.rectangle(600, 200, boxWidth, boxWidth, {
+    let boxJS = this.Bodies.rectangle(width/2, -110, boxWidth, boxWidth, {
       label: 'JavaScript',
-      angle: 2,
+      angle: Math.floor(Math.random() * Math.floor(5)),
+      isStatic: true,
       render: {
         sprite: {
           texture: 'javascript720.png',
@@ -149,9 +222,10 @@ class App extends React.Component {
         }
       }
     });
-    let boxHTML = this.Bodies.rectangle(600, 200, boxWidth, boxWidth, {
+    let boxHTML = this.Bodies.rectangle(width/2.5, -110, boxWidth, boxWidth, {
       label: 'HTML',
       angle: 2,
+      isStatic: true,
       render: {
         sprite: {
           texture: 'html300.png',
@@ -160,8 +234,9 @@ class App extends React.Component {
         }
       }
     });
-    let circleGit = this.Bodies.circle(width/2.7, height/3, boxWidth/2, {
+    let circleGit = this.Bodies.circle(50, -110, boxWidth/2, {
       label: 'Github',
+      isStatic: true,
       render: {
         sprite: {
           texture: 'github.svg',
@@ -170,8 +245,9 @@ class App extends React.Component {
         }
       }
     })
-    let boxSQL = this.Bodies.rectangle(650, 50, boxWidth, boxWidth, {
+    let boxSQL = this.Bodies.rectangle(width/2, -110, boxWidth, boxWidth, {
       label: 'mySQL',
+      isStatic: true,
       render: {
         sprite: {
           texture: 'mysql.jpg',
@@ -180,9 +256,10 @@ class App extends React.Component {
         }
       }
     });
-    let boxReact = this.Bodies.rectangle(width*.7, 10, boxWidth, boxWidth, {
+    let boxReact = this.Bodies.rectangle(width*.6, -110, boxWidth, boxWidth, {
       angle: 3,
       label: 'React',
+      isStatic: true,
       render: {
         sprite: {
           texture: 'react.svg',
@@ -191,20 +268,22 @@ class App extends React.Component {
         }
       }
     });
-    let boxAngular = this.Bodies.rectangle(width*.8, 25, boxWidth, boxWidth, {
+    let boxAngular = this.Bodies.rectangle(width*.3, -200, boxWidth, boxWidth, {
       label: 'Angular',
+      isStatic: true,
       angle:2,
       render: {
         sprite: {
           texture: 'angular.jpg',
-          xScale: boxWidth/250,
-          yScale: boxWidth/250
+          xScale: boxWidth/225,
+          yScale: boxWidth/225
         }
       }
     }) 
-    let boxMongo = this.Bodies.rectangle(300, 30, boxWidth, boxWidth, {
+    let boxMongo = this.Bodies.rectangle(width/1.3, -110, boxWidth, boxWidth, {
       label: 'MongoDB',
       angle: 1.5,
+      isStatic: true,
       render: {
         sprite: {
           texture: 'MongoDB.png',
@@ -213,9 +292,10 @@ class App extends React.Component {
         }
       }
     });
-    let boxJQuery = this.Bodies.rectangle(300, 30, boxWidth, boxWidth, {
+    let boxJQuery = this.Bodies.rectangle(width/3, -110, boxWidth, boxWidth, {
       label: 'jQuery',
       angle: 5,
+      isStatic: true,
       render: {
         sprite: {
           texture: 'jquery.jpg',
@@ -224,9 +304,10 @@ class App extends React.Component {
         }
       }
     });
-    let boxSublime = this.Bodies.rectangle(300, 30, boxWidth, boxWidth, {
+    let boxSublime = this.Bodies.rectangle(width/1.2, -110, boxWidth, boxWidth, {
       label: 'Sublime Text',
       angle: 5,
+      isStatic: true,
       render: {
         sprite: {
           texture: 'sublime.png',
@@ -235,41 +316,36 @@ class App extends React.Component {
         }
       }
     });
-
-    let mouse = this.Mouse.create(render.canvas)
-    let mouseConstraint = this.MouseConstraint.create(this.engine, {
-        mouse: mouse,
-          constraint: {
-            stiffness: 0.2,
-            render: {
-              visible: false
-            }
-          }
-      });
-    this.World.add(this.engine.world, [mouseConstraint, circleName, boxName, boxHTML, boxJS, boxSublime, circleGit, boxSQL, boxReact, boxAngular, boxMongo, boxJQuery, ground, leftWall, rightWall, ceiling]);
-    this.Events.on(mouseConstraint, "startdrag", (e) => {
-      this.setState({
-        startClickPos: {x: e.mouse.absolute.x, y: e.mouse.absolute.y}
-      })
-    })
-    this.Events.on(mouseConstraint, "enddrag", (e) => {
-      this.setState({
-        endClickPos: {x: e.mouse.absolute.x, y: e.mouse.absolute.y}
-      })
-      this.handleClick(e.body.label)
-    })
-    this.Engine.run(this.engine);
-    this.Render.run(render);
+    this.World.add(this.engine.world, [this.mouseConstraint, boxName, circleName, boxJS, boxHTML, circleGit, boxReact, boxAngular, boxSQL, boxMongo, boxJQuery, boxSublime, ground, leftWall, rightWall, ceiling])
+    setTimeout(() => {
+      this.Body.setStatic(boxJS, false)
+    },1000)
+    setTimeout(() => {
+      this.Body.setStatic(boxHTML, false)
+    },2000)
+    setTimeout(() => {
+      this.Body.setStatic(circleGit, false)
+    },2500)
+    setTimeout(() => {
+      this.Body.setStatic(boxReact, false)
+      this.Body.setStatic(boxAngular, false)
+    },3000)
+    setTimeout(() => {
+      this.Body.setStatic(boxJQuery, false)
+      this.Body.setStatic(boxMongo, false)
+      this.Body.setStatic(boxSQL, false)
+      this.Body.setStatic(boxSublime, false)
+    },3400)
   }
   render () {
     return (<div>
-      <Modal isOpen={this.state.showModal} style={{overlay: {animation: 'fadein .25s'}, content: {animation: 'fadein .4s', position:'absolute', textAlign: 'center'}}}>
+      <Modal isOpen={this.state.showModal} ariaHideApp={false} style={{overlay: {animation: 'fadein .25s'}, content: {animation: 'fadein .4s', position:'absolute', textAlign: 'center'}}}>
       <div style={{backgroundColor: '#eee', height: '90%', fontSize: '7vmin', overflowY:'scroll'}}>
       <p>{'I know ' + this.state.box}</p>
       </div>
       <button className="closeModal" onClick={() => {this.handleModal()}}>Close</button>
       </Modal>
-      <h1 className="name">Aaron<span></span><p className="webdev2">WEB DEV</p><p className="webdev">WEB DEV</p></h1><h1 className="tutorial">Click or drag an icon!</h1><button className="bullet" onClick={() => {this.handleBulletTime()}}>Activate Bullet Time</button></div>)
+      <h1 className="name">Aaron<span></span><p className="webdev2">WEB DEV</p><p className="webdev">WEB DEV</p></h1><h1 className="tutorial">Click or drag an icon!</h1><button className="bullet" onClick={() => {this.handleBulletTime()}}>Activate Bullet Time</button><button className="reload" onClick={() => {this.handleReload()}}>Restart Sandbox</button><button className="rain" onClick={() => {this.toggleRain()}}>☔</button></div>)
   }
 }
 
